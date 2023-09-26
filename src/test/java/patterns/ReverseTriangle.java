c533d0cac1ac4f095d34c6cc1d3f820d2452f22e
Ram Murthy Raju Vatsavai <86653234+sriramvarma09@users.noreply.github.com>
2023-09-26 15:59:14 +0530

    Update ReverseTriangle.java

--- src/test/java/patterns/ReverseTriangle.java
+++ src/test/java/patterns/ReverseTriangle.java
@@ -1,1 +1,24 @@
 //reverse Triangle code
+package patterns;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in)
+		int row=sc.nextInt();
+		for (int i = 0; i <=row; i++) {  //row
+	    	   
+			for (int j = row; j >=i; j--) {  //column
+	    		  
+				System.out.print(" ");
+				
+			}for (int j = 1; j <= i; j++) 
+			 {
+                System.out.print("* ");
+            }
+			 System.out.println();
+		}
+
+	}
+
+}
